9506a17001fc17fb689e66e74026ec21200ac599
Paramveer Singh <140582885+GirmadaShaw@users.noreply.github.com>
2023-10-12 02:40:09 +0530

    Add files via upload

---
+++ full-bloom-flowers.cpp
@@ -0,0 +1,69 @@
+/*
+You are given a 0-indexed 2D integer array flowers, where flowers[i] = [starti, endi] means the ith flower will be in full
+bloom from starti to endi (inclusive). You are also given a 0-indexed integer array people of size n, where people[i]
+is the time that the ith person will arrive to see the flowers.
+Return an integer array answer of size n, where answer[i] is the number of flowers that are in full bloom
+when the ith person arrives.
+
+Example 1 :
+Input: flowers = [[1,6],[3,7],[9,12],[4,13]], poeple = [2,3,7,11]
+Output: [1,2,2,2]
+Explanation: The figure above shows the times when the flowers are in full bloom and when the people arrive.
+For each person, we return the number of flowers in full bloom during their arrival.
+
+Example 2:
+Input: flowers = [[1,10],[3,3]], poeple = [3,3,2]
+Output: [2,2,1]
+Explanation: The figure above shows the times when the flowers are in full bloom and when the people arrive.
+For each person, we return the number of flowers in full bloom during their arrival.
+
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int countBloom( int time , vector<vector<int>> flower )
+{
+    int count = 0 , i = 0  ; 
+    while(  i < flower.size() )
+    {
+        if( flower[i][0] <= time && time <= flower[i][1] )
+            count ++ ; 
+        i ++ ;    
+    }
+    return count ; 
+}
+
+int main()
+{
+    // vector<vector<int>> flower =
+    //     {
+    //         {1, 6},
+    //         {3, 7},
+    //         {9, 12},
+    //         {4,13}
+
+    //     };
+
+        vector<vector<int>> flower =
+        {
+            {1, 10},
+            {3, 3},
+
+        };
+    
+    //vector<int> people = { 2,3,7,11 } ;
+    vector<int> people = { 3,3,2 } ;
+    
+    vector<int> answer ; 
+
+    for( int i = 0 ; i < people.size() ; i ++ )
+        answer.push_back( countBloom( people[i] , flower ) ) ;
+
+    cout  << "Number of Blooming flowers: " ;
+    for( int i = 0 ; i < people.size() ; i ++ )
+        cout << answer[i] << " " ;
+
+}
